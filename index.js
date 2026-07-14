6a1e1458d5dba22c37de33e82539e9f23bb21d87
samchat2561 <samchat2561@gmail.com>
2026-07-14 11:27:40 +0700

    update

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 const express = require('express');
 const app = express();
 
+//Data mockup
+const users = [
+    { id: '101', name: "Somchai", role: "Admin" },
+    { id: '102', name: "Somsri", role: "USer" },
+]
+
 app.get('/', (req, res) => {
     return res.send(`สวัสดียามเช้า`);
+});
+
+app.get('/users/:id', (req, res) => {
+    const user = users.find((u) => u.id === req.params.id);
+    user ? res.status(200).json(user) : res.status(404).json({ error: "Not Found" })
 });
 
 const PORT = 3000;
